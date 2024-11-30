749921458f0b11c6992273c3ad64c576a4d5a3f6
Phalluis <nicholaslonganilla@gmail.com>
2024-11-30 16:48:45 +0800

    tite

--- staffhomepage.php
+++ staffhomepage.php
@@ -12,21 +12,21 @@
 	<title>Staff Dashboard</title>
 <style>
 	ul {
-  	list-style-type: none;
- 	margin: 0;
-  	padding: 0;
-  	width: 10%;
-  	background-color: #f1f1f1;
-  	position: fixed;
-  	height: 100%;
-  	overflow: auto;
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+		width: 10%;
+		background-color: #f1f1f1;
+		position: fixed;
+		height: 100%;
+		overflow: auto;
 	}
 
 	li a {
-  	display: block;
-  	color: #000;
-  	padding: 8px 16px;
-  	text-decoration: none;
+		display: block;
+		color: #000;
+		padding: 8px 16px;
+		text-decoration: none;
 	}
 
 	li a.active {
@@ -40,11 +40,12 @@
 	}
 
 	form input {
-  	display: block;
-  	color: #000;
-  	padding: 8px 16px;
-  	text-decoration: none;
+		display: block;
+		color: #000;
+		padding: 8px 16px;
+		text-decoration: none;
 	}
+
 </style>
 </head>
 <?php
@@ -60,7 +61,7 @@
 		<li><a href="#product">Product</a></li>
 		<li><a href="#material">Material</a></li>
 		<li><a href="#report">Report</a></li>
-		<form method="POST"><input type="submit" name="logout">
+		<form method="POST"><input type="submit" name="logout" value="Logout">
 	</ul>
 </body>
 </html>
